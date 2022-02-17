246edf421ce4eb5a30feffc1692bd8a221dc8734
Pablo <pabmun02@ucm.es>
2022-02-17 11:49:28 +0100

    Correccion

--- 01l.py
+++ 01l.py
@@ -1,32 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 14 18:05:10 2022
+Created on Thu Feb 17 11:51:45 2022
 
-@author: pavmb
+@author: pablo
 """
 
-from multiprocessing import Process, BoundedSemaphore
+from multiprocessing import Process, Lock
 from multiprocessing import current_process
 from multiprocessing import Value, Array
 N = 8
-def task(common, tid, semaphore):
+def task(common, tid, lock):
  a = 0
  for i in range(100):
      print(f'{tid}−{i}: Non−critical Section')
      a += 1
      print(f'{tid}−{i}: End of non−critical Section')
-     semaphore.acquire()
-     print(f'{tid}−{i}: Critical section')
-     v = common.value + 1
-     print(f'{tid}−{i}: Inside critical section')
-     common.value = v
-     print(f'{tid}−{i}: End of critical section')
-     semaphore.release()
+     try:
+         lock.acquire()
+         print(f'{tid}−{i}: Critical section')
+         v = common.value + 1
+         print(f'{tid}−{i}: Inside critical section')
+         common.value = v
+         print(f'{tid}−{i}: End of critical section')
+     finally:
+         lock.release()
 def main():
  lp = []
  common = Value('i', 0)
+ lock = Lock()
  for tid in range(N):
-     lp.append(Process(target=task, args=(common, tid, semaphore)))
+     lp.append(Process(target=task, args=(common, tid, lock)))
  print (f"Valor inicial del contador {common.value}")
  for p in lp:
      p.start()
@@ -35,5 +38,4 @@ def main():
  print (f"Valor final del contador {common.value}")
  print ("fin")
 if __name__ == "__main__":
- semaphore = BoundedSemaphore(1)
  main()
--- 01s.py
+++ 01s.py
@@ -2,31 +2,34 @@
 """
 Created on Mon Feb 14 18:05:10 2022
 
-@author: pavmb
+@author: pablo
 """
 
-from multiprocessing import Process, Lock
+from multiprocessing import Process, BoundedSemaphore
 from multiprocessing import current_process
 from multiprocessing import Value, Array
 N = 8
-def task(common, tid, lock):
+def task(common, tid, semaphore):
  a = 0
  for i in range(100):
      print(f'{tid}−{i}: Non−critical Section')
      a += 1
      print(f'{tid}−{i}: End of non−critical Section')
-     lock.acquire()
-     print(f'{tid}−{i}: Critical section')
-     v = common.value + 1
-     print(f'{tid}−{i}: Inside critical section')
-     common.value = v
-     print(f'{tid}−{i}: End of critical section')
-     lock.release()
+     try:
+         semaphore.acquire()
+         print(f'{tid}−{i}: Critical section')
+         v = common.value + 1
+         print(f'{tid}−{i}: Inside critical section')
+         common.value = v
+         print(f'{tid}−{i}: End of critical section')
+     finally:
+         semaphore.release()
 def main():
  lp = []
  common = Value('i', 0)
+ semaphore = BoundedSemaphore(1)
  for tid in range(N):
-     lp.append(Process(target=task, args=(common, tid, lock)))
+     lp.append(Process(target=task, args=(common, tid, semaphore)))
  print (f"Valor inicial del contador {common.value}")
  for p in lp:
      p.start()
@@ -35,5 +38,4 @@ def main():
  print (f"Valor final del contador {common.value}")
  print ("fin")
 if __name__ == "__main__":
- lock = Lock()
  main()
